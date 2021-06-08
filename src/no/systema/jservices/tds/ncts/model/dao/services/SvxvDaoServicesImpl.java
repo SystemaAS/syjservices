b24e8542422b419db5b81a4fb772a54b734cabf7
oscardelatorre <oscardelatorre@localhost>
2021-06-08 14:22:29 +0200

    Update Svxvam for deleteAll

--- src/no/systema/jservices/tds/ncts/model/dao/services/SvxvDaoServicesImpl.java
+++ src/no/systema/jservices/tds/ncts/model/dao/services/SvxvDaoServicesImpl.java
@@ -40,6 +40,10 @@ public class SvxvDaoServicesImpl implements SvxvDaoServices {
 			logger.warn(sql.toString());
 			//params
 			retval = this.jdbcTemplate.update( sql.toString(), new Object[] { dao.getTvavd(), dao.getTvtdn() } );
+			if(retval>=0) {
+				logger.warn("deleting children on: svxvam ...");
+				retval = this.deleteAllChildren(dao.getTvavd(), dao.getTvtdn(), errorStackTrace);
+			}
 			
 		}catch(Exception e){
 			Writer writer = this.dbErrorMessageMgr.getPrintWriter(e);
@@ -51,6 +55,40 @@ public class SvxvDaoServicesImpl implements SvxvDaoServices {
 
 		return retval;
 	}
+	
+	/**
+	 * Children tables here
+	 * @param avd
+	 * @param opd
+	 * @param errorStackTrace
+	 * @return
+	 */
+	private int deleteAllChildren(String avd, String opd, StringBuffer errorStackTrace){
+		int retval = 0;
+		try{
+				
+			StringBuffer sql = new StringBuffer();
+			//DEBUG --> logger.info("mydebug");
+			sql.append(" DELETE from svxvam ");
+			sql.append(" WHERE tvavd = ? ");
+			sql.append(" AND tvtdn = ? ");
+			logger.warn(sql.toString());
+			//params
+			retval = this.jdbcTemplate.update( sql.toString(), new Object[] { avd, opd } );
+			
+			
+		}catch(Exception e){
+			Writer writer = this.dbErrorMessageMgr.getPrintWriter(e);
+			logger.info(writer.toString());
+			//Chop the message to comply to JSON-validation
+			errorStackTrace.append(this.dbErrorMessageMgr.getJsonValidDbException(writer));
+			retval = -1;
+		}
+
+		return retval;
+	}
+	
+	
 	
 	/**                                                                                                  
 	 * Wires jdbcTemplate                                                                                
